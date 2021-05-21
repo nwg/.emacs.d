ea59e9c6e40c04df20b48b82937a66721e18cdb1
Nathaniel Griswold <nategriswold@gmail.com>
2021-05-21 12:30:12 -0500

    add some bindings for dired

--- init.el
+++ init.el
@@ -176,6 +176,9 @@
 
 (use-package dired-x
   :after dired
+  :bind (:map dired-mode-map
+              ("s-<up>" . dired-up-directory)
+              ("s-<down>" . dired-find-file))
   :config
 
   (setq dired-omit-files
@@ -193,16 +196,10 @@
 
   (defun nwg/dired-new-file ()
     (interactive)
-    (let* ((fn "New File")
-           (buf (generate-new-buffer fn)))
-      (switch-to-buffer buf)))
-
-  (defun nwg/dired-new-file-2 ()
-    (interactive)
     (let* ((buf (current-buffer)))
       (call-interactively #'find-file)))
 
-  (define-key dired-mode-map (kbd "n") #'nwg/dired-new-file-2)
+  (define-key dired-mode-map (kbd "n") #'nwg/dired-new-file)
 
   (defun nwg/setup-dired ()
     (dired-omit-mode 1))
@@ -396,3 +393,9 @@
  '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button))))))
 
 (provide 'init)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((racket-user-command-line-arguments "-I" "racket"))))
