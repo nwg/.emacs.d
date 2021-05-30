bc9ac4849cbf3f757faad5d701bc709039da14d7
Nathaniel Griswold <nategriswold@gmail.com>
2021-05-29 20:17:09 -0500

    fix abort bug in chained capture

--- library/nwg-util/lisp/nwg-org.el
+++ library/nwg-util/lisp/nwg-org.el
@@ -8,14 +8,15 @@
   (org-add-link-type "message" 'open-message-link))
 
 (defun nwg/capture-finalize ()
-  (let* ((template (alist-get nwg/last-capture-quit nwg-exit-templates-alist)))
-    (when (and (not org-note-abort) template)
-      (with-current-buffer (marker-buffer org-capture-last-stored-marker)
-        (save-excursion
-          (goto-char (marker-position org-capture-last-stored-marker))
+  (when (not org-note-abort)
+    (let* ((template (alist-get nwg/last-capture-quit nwg-exit-templates-alist)))
+      (when template
+        (with-current-buffer (marker-buffer org-capture-last-stored-marker)
+          (save-excursion
+            (goto-char (marker-position org-capture-last-stored-marker))
 
-          (let ((org-capture-templates (list (append (list "*" "Automatic") template))))
-            (org-capture nil "*")))))))
+            (let ((org-capture-templates (list (append (list "*" "Automatic") template))))
+              (org-capture nil "*"))))))))
 
 (defun nwg/capture-before-finalize ()
   (setq nwg/last-capture-quit (org-capture-get :exit t)))
